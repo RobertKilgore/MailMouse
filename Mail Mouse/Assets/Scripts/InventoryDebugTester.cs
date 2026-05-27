26fc2890eebe7ee047ef4072065bd31e9fadbc93
Robert J Kilgore <robertjoeykilgore@gmail.com>
2026-05-26 22:13:36 -0400

    Update InventoryDebugTester.cs

--- Mail Mouse/Assets/Scripts/InventoryDebugTester.cs
+++ Mail Mouse/Assets/Scripts/InventoryDebugTester.cs
@@ -2,15 +2,64 @@ using UnityEngine;
 
 public class InventoryDebugTester : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public InventoryGrid grid;
+
+    public InventoryItem testItem;
+
+    public Vector2Int testPosition;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            TryPlace();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            grid.RemoveItem(testItem);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            testItem.RotateClockwise();
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            MoveToMousePosition();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void TryPlace()
     {
-        
+        if (grid.PlaceItem(testPosition, testItem))
+        {
+            Debug.Log("Placed item at " + testPosition);
+        }
+        else
+        {
+            Debug.Log("FAILED to place item at " + testPosition);
+        }
+    }
+
+    private void MoveToMousePosition()
+    {
+        Vector2 mouse = Input.mousePosition;
+
+        Vector2 localPoint;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            grid.GetComponent<RectTransform>(),
+            mouse,
+            null,
+            out localPoint
+        );
+
+        int x = Mathf.FloorToInt(localPoint.x / 64f);
+        int y = Mathf.FloorToInt(-localPoint.y / 64f);
+
+        testPosition = new Vector2Int(x, y);
+
+        Debug.Log("Mouse grid position: " + testPosition);
     }
 }
